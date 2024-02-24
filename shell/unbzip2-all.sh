1badbbabe13cd8f54741b5bbda6c294332ff574b
juandazeng <juanda@gmail.com>
2024-02-25 02:23:54 +0800

    a

--- shell/unbzip2-all.sh
+++ shell/unbzip2-all.sh
@@ -2,30 +2,28 @@ echo ----------------------------------------------
 echo Unzipping bzip2 files...
 echo ----------------------------------------------
 
-for category in cluster master worker
+category=$1
+echo category:$category
+getAllResultsDirectories=`ls $category/ | egrep -i '[0-9]'`
+for d in $getAllResultsDirectories
 do
-  echo category:$category
-  getAllResultsDirectories=`ls $category/ | egrep -i '[0-9]'`
-  for d in $getAllResultsDirectories
-  do
-    d=$category/$d
-    echo directory:$d
-    bzip2FileCount=`ls -1 $d/*.bzip2 2>/dev/null | wc -l`
-    if [ $bzip2FileCount -gt 0 ]
-    then
-      for f in $d/*.bzip2
-      do
-        localNameExt=$(basename $f)
-        localName=${localNameExt%.*}
-        targetName=$d-$localName
-        echo targetName:$targetName
-        bunzip2 -c $f > $targetName.xml
-        # oscap xccdf generate report $targetName.xml > $targetName.html
-        # python3 ../shell/arf2csv.py $targetName.xml
-      done
-    fi
-    rm -rf $d
-  done
+  d=$category/$d
+  echo directory:$d
+  bzip2FileCount=`ls -1 $d/*.bzip2 2>/dev/null | wc -l`
+  if [ $bzip2FileCount -gt 0 ]
+  then
+    for f in $d/*.bzip2
+    do
+      localNameExt=$(basename $f)
+      localName=${localNameExt%.*}
+      targetName=$d-$localName
+      echo targetName:$targetName
+      bunzip2 -c $f > $targetName.xml
+      # oscap xccdf generate report $targetName.xml > $targetName.html
+      # python3 ../shell/arf2csv.py $targetName.xml
+    done
+  fi
+  rm -rf $d
 done
 
 echo
