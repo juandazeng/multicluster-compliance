2efd7389d95f170a0cd63c6ed3d130609897a264
juandazeng <juanda@gmail.com>
2024-02-25 16:55:16 +0800

    a

--- shell/unbzip2-all.sh
+++ shell/unbzip2-all.sh
@@ -5,17 +5,17 @@ echo ----------------------------------------------
 clusterName=$1
 category=$2
 getAllResultsDirectories=`ls $category/ | egrep -i '[0-9]'`
-for d in $getAllResultsDirectories
+for numberedDirectory in $getAllResultsDirectories
 do
-  d=$category/$d
-  bzip2FileCount=`ls -1 $d/*.bzip2 2>/dev/null | wc -l`
+  targetDirectory=$category/$numberedDirectory
+  bzip2FileCount=`ls -1 $targetDirectory/*.bzip2 2>/dev/null | wc -l`
   if [ $bzip2FileCount -gt 0 ]
   then
-    for f in $d/*.bzip2
+    for f in $targetDirectory/*.bzip2
     do
       localNameExt=$(basename $f)
-      localName=${localNameExt%.*}
-      targetName=$d-$localName
+      localName=$numberedDirectory-${localNameExt%.*}
+      targetName=$category/$localName
       echo source:$f
       echo target:$targetName.xml
       bunzip2 -c $f > $targetName.xml
