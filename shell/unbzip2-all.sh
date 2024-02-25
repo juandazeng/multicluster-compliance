7c1159664b32fde96dbfe971feabc3f77997a39f
juandazeng <juanda@gmail.com>
2024-02-25 16:42:31 +0800

    a

--- shell/unbzip2-all.sh
+++ shell/unbzip2-all.sh
@@ -16,9 +16,11 @@ do
       localNameExt=$(basename $f)
       localName=${localNameExt%.*}
       targetName=$d-$localName
+      echo source:$f
+      echo target:$targetName.xml
       bunzip2 -c $f > $targetName.xml
       # oscap xccdf generate report $targetName.xml > $targetName.html
-      python3 ../shell/arf2csv.py --cluster $clusterName --target $category --input $targetName.xml
+      python3 ../shell/arf2csv.py --cluster $clusterName --target $category --input $localName.xml
     done
   fi
   rm -rf $d
